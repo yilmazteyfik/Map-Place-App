dd8aaffded27992abdf794bbfaf6e26566726342
yilmazteyfik <teyfik.yilmaz@bahcesehir.edu.tr>
2024-05-08 13:35:37 +0300

    first commit

---
+++ Map-Place-App/Map-Place-App/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Map-Place-App
+//
+//  Created by Teyfik Yılmaz on 8.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
