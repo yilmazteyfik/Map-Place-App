77ef47778e3e098c289f7c930514c502602eaca6
Osmancan Akagündüz <83430594+osmancan23@users.noreply.github.com>
2024-05-13 14:50:45 +0300

    merged bartugs codes

---
+++ Map-Place-App/Map-Place-App/Controller/CellController/PlaceCell.swift
@@ -0,0 +1,38 @@
+//
+//  PlaceCell.swift
+//  Location-Search
+//
+//  Created by Bartuğ Kaan Çelebi on 4.04.2024.
+//
+
+import UIKit
+
+class PlaceCell: UITableViewCell {
+
+    @IBOutlet weak var placeDetailImage: UIImageView!
+    @IBOutlet weak var placeNameLabel: UILabel!
+    @IBOutlet weak var placeDetailLabel: UILabel!
+    @IBOutlet weak var placeDetailButton: UIButton!
+    
+    static let identifier = "placeCell"
+    
+    static func nib() -> UINib{
+        return UINib(nibName: "PlaceCell", bundle: nil)
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        selectionStyle = .none
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    @IBAction func viewDetailTapped(_ sender: UIButton) {
+        //TODO: Send to Place Detail Screen with datas (İd ??)
+        
+    }
+}
--- Map-Place-App/Map-Place-App/Controller/PlaceView/PlaceListDetailViewController.swift
+++ Map-Place-App/Map-Place-App/Controller/PlaceView/PlaceListDetailViewController.swift
@@ -9,11 +9,42 @@ import Foundation
 import UIKit
 
 class PlaceListDetailViewController: UIViewController {
-
+    //var placeArray = [Place]()
+    @IBOutlet weak var searchbarView: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(PlaceCell.nib(), forCellReuseIdentifier: PlaceCell.identifier)
+        tableView.separatorColor = .clear
+        searchbarView.delegate = self
     }
+}
 
+//MARK: - TableView Delegate and DataSource
+extension PlaceListDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: PlaceCell.identifier, for: indexPath) as! PlaceCell
+        //Veriye Göre değişicek
+        cell.placeNameLabel.text = "Place \(indexPath.row + 1)"
+        cell.placeDetailImage.image = UIImage(systemName: "house")
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let detailVC = self.storyboard?.instantiateViewController(withIdentifier: "PlaceDetailViewController") as PlaceDe
+    }
+    
+}
 
+extension PlaceListDetailViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //TODO: Arraydeki elemanlardan isim araması yapıp tableView'ı reload edicek
+    }
 }
